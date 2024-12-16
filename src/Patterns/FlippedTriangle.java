a9e4244119e9fefec3c0a4414b6111bd0d7290f8
Kushagra0320 <kushagra0320@gmail.com>
2024-12-16 20:12:36 +0530

    5.july.2024

---
+++ src/Patterns/FlippedTriangle.java
@@ -0,0 +1,22 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class FlippedTriangle {
+    public static void main(String[] args) {
+            System.out.println("Enter rows and columns");
+            Scanner Sc=new Scanner(System.in);
+            int m = Sc.nextInt();//rows and cols
+            for(int i=1;i<=m;i++){
+                for(int j=1;j<=m;j++){ //for flipped triangle j<=m
+                    if(i+j>m)
+                        System.out.print("*"+" ");
+                    else
+                        System.out.print(" "+" ");
+                }
+                System.out.println();// for next line
+            }
+
+        }
+    }
+
