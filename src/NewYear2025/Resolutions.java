067aeb966ab5beed45970769454f6c6afb922d94
Kushagra0320 <kushagra0320@gmail.com>
2025-01-01 02:57:31 +0530

    5.july.2024

---
+++ src/NewYear2025/Resolutions.java
@@ -0,0 +1,18 @@
+package NewYear2025;
+
+public class Resolutions {
+    public static void main(String[] args) {
+        System.out.println("BE THE ONE YOU ALWAYS WANT TO BE!!");
+        System.out.println(" 1.  Placement:");
+        System.out.println("Data Structures and Algo");
+        System.out.println("Aptitude For MBA");
+        System.out.println("Java Backend Development");
+        System.out.println("Core Subjects,CyberSecurity Fundamentals,MongoDb,Data Analytics,Case Studies");
+        System.out.println("2.  MBA");
+        System.out.println("CFA , CAT and Stanford etc");
+        System.out.println("3.  Daily Lifestyle");
+        System.out.println("Read Book,Drink Water,Exercise Daily,Eat Healthy,Podcast");
+        System.out.println("T A K E . C H A R G E . O F .  Y O U R .  D R E A M S");
+
+    }
+}
